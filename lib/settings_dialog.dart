20ce2fcad2dce22b715f7e6a8af6f5da54fe7246
Alperen Aksoy <alperen.aksoy@alumni.fh-aachen.de>
2023-12-03 19:29:08 +0100

    added const

--- lib/settings_dialog.dart
+++ lib/settings_dialog.dart
@@ -17,8 +17,8 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text("Notifications"),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+      title: const Text("Notifications"),
+      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
       content: Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.start,
@@ -55,52 +55,6 @@ class _SettingsState extends State<Settings> {
         TextButton(
           child: const Text('Cancel'),
           onPressed: () => Navigator.of(context).pop(),
-        ),
-      ],
-
-    );
-    return CupertinoAlertDialog(
-      title: const Text(
-        "Notifications",
-        style: TextStyle(fontWeight: FontWeight.bold),
-      ),
-      content: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            RadioMenuButton(
-                value: true,
-                groupValue: notifications,
-                onChanged: (value) => setState(() {
-                      notifications = value!;
-                    }),
-                child: const Text("On")),
-            RadioMenuButton(
-                value: false,
-                groupValue: notifications,
-                onChanged: (value) => setState(() {
-                      notifications = value!;
-                    }),
-                child: const Text("Off")),
-          ]),
-      actions: [
-        CupertinoDialogAction(
-            child: const Text("save"),
-            onPressed: (){
-              prefs.setBool("notifications", notifications);
-              if(notifications) {
-                Notify.prayerTimesNotifiyAll(pt);
-              } else {
-                Notify.cancelNotifications();
-              }
-              Navigator.of(context, rootNavigator: true).pop();
-            }
-        ),
-        CupertinoDialogAction(
-          child: const Text("cancel"),
-          onPressed: () => Navigator.of(context, rootNavigator: true).pop(),
-
         ),
       ],
     );
