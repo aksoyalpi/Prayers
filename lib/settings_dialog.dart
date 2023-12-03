91452054069df3346858d540149e12323ec237e7
Alperen Aksoy <alperen.aksoy@alumni.fh-aachen.de>
2023-12-03 19:26:02 +0100

    Changed CupertinoAlertDialog to AlertDialog

--- lib/settings_dialog.dart
+++ lib/settings_dialog.dart
@@ -16,6 +16,49 @@ class _SettingsState extends State<Settings> {
 
   @override
   Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Notifications"),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+      content: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RadioMenuButton(
+                value: true,
+                groupValue: notifications,
+                onChanged: (value) => setState(() {
+                  notifications = value!;
+                }),
+                child: const Text("On")),
+            RadioMenuButton(
+                value: false,
+                groupValue: notifications,
+                onChanged: (value) => setState(() {
+                  notifications = value!;
+                }),
+                child: const Text("Off")),
+          ]),
+      actions: [
+        TextButton(
+          child: const Text('Save'),
+            onPressed: (){
+              prefs.setBool("notifications", notifications);
+              if(notifications) {
+                Notify.prayerTimesNotifiyAll(pt);
+              } else {
+                Notify.cancelNotifications();
+              }
+              Navigator.of(context).pop();
+            },
+        ),
+        TextButton(
+          child: const Text('Cancel'),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ],
+
+    );
     return CupertinoAlertDialog(
       title: const Text(
         "Notifications",
